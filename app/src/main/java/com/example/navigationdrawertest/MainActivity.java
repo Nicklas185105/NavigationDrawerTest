ee70a4b385e476b2d20714e5f1cbfb86564d2ce9
Nicklas185105 <s185105@student.dtu.dk>
2019-11-05 17:07:30 +0100

    test

--- app/src/main/java/com/example/navigationdrawertest/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawertest/MainActivity.java
@@ -62,18 +62,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_map:
                 getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
                         .replace(R.id.fragment_container, new MapFragment()).commit();
+                getSupportActionBar().setTitle("Kort");
                 break;
             case R.id.nav_ranklist:
                 getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
                         .replace(R.id.fragment_container, new RanklisteFragment()).commit();
+                getSupportActionBar().setTitle("Rangliste");
                 break;
             case R.id.nav_about:
                 getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
                         .replace(R.id.fragment_container, new AboutFragment()).commit();
+                getSupportActionBar().setTitle("Om Os");
                 break;
             case R.id.nav_scanner:
                 getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
                         .replace(R.id.fragment_container, new ScannerFragment()).commit();
+                getSupportActionBar().setTitle("QR Scanner");
                 break;
             case R.id.nav_back:
                 super.onBackPressed();
